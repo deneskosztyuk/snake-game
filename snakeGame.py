7c98c599da569a7456fdfa0f16d55b84105aefb5
Denes <denes.kosztyuk@gmail.com>
2022-12-09 00:17:42 +0000

    python snake game

---
+++ snakeGame.py
@@ -0,0 +1,17 @@
+import pygame
+import time
+import random
+
+snake_speed = 15
+
+window_x = 720
+window_y = 480
+
+# RGB
+black = pygame.Color.Color(0, 0, 0)
+white = pygame.Color.Color(255, 255, 255)
+red = pygame.Color.Color(255, 0, 0)
+green = pygame.Color.Color(0, 255, 0)
+blue = pygame.Color.Color(0, 0, 255)
+
+
